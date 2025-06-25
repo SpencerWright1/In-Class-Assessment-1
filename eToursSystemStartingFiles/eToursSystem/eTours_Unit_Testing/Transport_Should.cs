ecbcd866e15b3c09769e45ef28acc3f70ed74d9c
Spencer Wright <swright41n@gmail.com>
2025-06-24 20:20:55 -0600

    Activity 2 Unit Tests Complete - After completion of the Unit tests for Activity 2

--- eToursSystemStartingFiles/eToursSystem/eTours_Unit_Testing/Transport_Should.cs
+++ eToursSystemStartingFiles/eToursSystem/eTours_Unit_Testing/Transport_Should.cs
@@ -12,6 +12,38 @@ namespace eTours_Unit_Testing
     {
         //TODO: Activity 2
         //place your unit tests here
+        [Fact]
+        public void Store_Valid_Name_And_Capacity()
+        {
+            var t = new Transport("StAlbertTransit", 40);
+            t.Name.Should().Be("StAlbertTransit");
+            t.Capacity.Should().Be(40);
+            t.ToString().Should().Be("StAlbertTransit,40");
+        }
+
+        [Fact]
+        public void Throw_ArgumentNullException_When_Name_Is_Null()
+        {
+            Action act = () => new Transport(null, 40);
+            act.Should().Throw<ArgumentNullException>()
+                .WithMessage("*Name is required*");
+        }
+
+        [Fact]
+        public void Throw_ArgumentException_When_Name_Is_Empty()
+        {
+            Action act = () => new Transport("   ", 40);
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("*Name cannot be empty*");
+        }
+
+        [Fact]
+        public void Throw_ArgumentException_When_Capacity_Is_Invalid()
+        {
+            Action act = () => new Transport("StAlbertTransit", 0);
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("*Capacity must be a positive number*");
+        }
 
     }
 }
