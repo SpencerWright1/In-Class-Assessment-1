655386299a6f1b66c31f54fb0298e113124cee77
Spencer Wright <swright41n@gmail.com>
2025-06-24 20:36:45 -0600

    Activity 2 Class Complete - After completion of the Class for Activity 2

--- eToursSystemStartingFiles/eToursSystem/eToursLib/Transport.cs
+++ eToursSystemStartingFiles/eToursSystem/eToursLib/Transport.cs
@@ -11,11 +11,44 @@ namespace eToursLib
         //TODO: Activity 2 place your code here
 
         // data members
-
+        private string _Name;
+        private int _Capacity;
 
         //properties
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(Name), "Name is required.");
+                value = value.Trim();
+                if (value == "")
+                    throw new ArgumentException("Name cannot be empty or whitespace.", nameof(Name));
+                _Name = value;
+            }
+        }
 
-
+        public int Capacity
+        {
+            get => _Capacity;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException($"Capacity must be a positive number. You entered: {value}", nameof(Capacity));
+                _Capacity = value;
+            }
+        }
         //methods
+        public override string ToString()
+        {
+            return $"{Name},{Capacity}";
+        }
+        //contructor
+        public Transport(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+        }
     }
 }
